cac0bab664a7adb9f6c2111102c036e152483350
suqinglin113x <2413561757@qq.com>
2016-11-02 15:14:19 +0800

    修改界面
    没大变动

--- Remote ios 10/ViewController.m
+++ Remote ios 10/ViewController.m
@@ -23,8 +23,6 @@
   
 
     
-    
-    
     //创建UI
     [self createUI];
 }
@@ -33,27 +31,24 @@
 - (void)createUI
 {
     _scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:_scrollView];
     _scrollView.backgroundColor = [UIColor brownColor];
-    _scrollView.contentSize = CGSizeMake(300, 2000);
-    
-    _scrollView.refreshControl = [[UIRefreshControl alloc] init];
-    [_scrollView.refreshControl setAttributedTitle:[[NSAttributedString alloc] initWithString:@"下拉刷新"]];
-    [_scrollView.refreshControl beginRefreshing];
-    _scrollView.refreshControl.tintColor = [UIColor redColor];
-    
-//    [self.view addSubview:_scrollView];
-    
-    
-    UILabel *label = [[UILabel alloc] init];
-    label.frame = CGRectMake(10, 100, 100, 30);
-    label.adjustsFontForContentSizeCategory = YES;
-    label.backgroundColor = [UIColor colorWithDisplayP3Red:0.4 green:0.3 blue:0.5 alpha:1];
-    label.text = @"我是iOS10label";
-    [_scrollView addSubview:label];
+    _scrollView.contentSize = CGSizeMake(self.view.bounds.size.width, self.view.bounds.size.height + 50);
+    _scrollView.contentInset = UIEdgeInsetsMake(0, 50, 00, 0);
+//    _scrollView.bounces = NO;
+    _scrollView.indicatorStyle = UIScrollViewIndicatorStyleWhite;
+//    _scrollView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 50, 0, 0);
+    [_scrollView flashScrollIndicators];
+    _scrollView.directionalLockEnabled = YES; //很有用
     
     
-    _tableView = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStylePlain];
-//    [self.view addSubview:_tableView];
+    UIButton *button = [[UIButton alloc] init];
+    button.frame = CGRectMake(10, 100, 150, 30);
+    button.backgroundColor = [UIColor colorWithDisplayP3Red:0.4 green:0.3 blue:0.5 alpha:1];
+    [button setTitle:@"我是iOS10label" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(jumpToSystemSetting) forControlEvents:UIControlEventTouchUpInside];
+    [_scrollView addSubview:button];
+    
     
     //添加刷新
     UIRefreshControl * _refreshControl = [[UIRefreshControl alloc] init];
@@ -62,21 +57,26 @@
                         action:@selector(refreshView:)
               forControlEvents:UIControlEventValueChanged];
     [_refreshControl setAttributedTitle:[[NSAttributedString alloc] initWithString:@"松手更新数据"]];
-    [_tableView addSubview:_refreshControl];
+    [_scrollView addSubview:_refreshControl];
     
 }
 
 - (void)refreshView:(UIRefreshControl *)refresh
 {
-    NSLog(@"ffffff");
-    [refresh endRefreshing];
+    NSLog(@"开始刷新！");
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        [refresh endRefreshing];
+        NSLog(@"结束刷新！");
+        
+    });
 }
 
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+
+- (void)jumpToSystemSetting
 {
-    NSLog(@"已点击");
+    NSLog(@"跳转到设置！");
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString] options:@{} completionHandler:nil];
 }
-
 @end
